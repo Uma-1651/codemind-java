e43fab5ac16ca74aac7779f5003f48297bdf23f1
Umesh Hemanth Shivaji <122421297+Uma-1651@users.noreply.github.com>
2024-02-06 21:21:09 +0530

    import java.util.*;
    public class RepeatedKTimes {
        public static void main(String[] args) {
            Scanner scanner = new Scanner(System.in);
            int n = scanner.nextInt();
            int x = scanner.nextInt();
            System.out.printf("%d
    %d",x,n);
        }
    }

---
+++ Swap_Two_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class RepeatedKTimes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int x = scanner.nextInt();
+        System.out.printf("%d
+%d",x,n);
+    }
+}
